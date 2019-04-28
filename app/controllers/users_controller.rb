a077162ad75b115ca88e3aa32170ecf55cf7c00a
Kian Edalat <akedalat@gmail.com>
2019-04-28 11:48:11 +0100

    RESTful user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,41 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show,:update,:destroy]
+
+  def index
+    users = User.all
+    render json: users, status: 200
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: 201
+    else
+      render json:{errors: user.errors.full_messages}
+    end
+  end
+
+  def update
+    @user.update(user_params)
+    render json: @user, status: 200
+  end
+
+  def destroy
+    @user.destroy
+    render json: {message:"user successfully deleted :("}
+  end
+
+  def show
+    render json: @user, status: 200
+  end
+
+  private
+  def user_params
+    params.permit(:name, :email, :avatar, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
