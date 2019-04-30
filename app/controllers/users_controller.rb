4a90ff8d84b4d7ee4e3569592fe1cd50d5d09611
Kian Edalat <akedalat@gmail.com>
2019-04-30 10:27:52 +0100

    password column deleted from users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.permit(:name, :email, :avatar, :password)
+    params.permit(:name, :email, :avatar)
   end
 
   def set_user
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_29_105607) do
+ActiveRecord::Schema.define(version: 2019_04_30_092151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,7 +47,6 @@ ActiveRecord::Schema.define(version: 2019_04_29_105607) do
     t.string "name"
     t.string "avatar"
     t.string "email"
-    t.string "password"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
