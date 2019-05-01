7a20fa5aa33b2c78e076783677b572794a9e9297
Kian Edalat <akedalat@gmail.com>
2019-05-01 13:35:40 +0100

    login action

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  #has_secure_password
+  has_secure_password
   validates :name, :email, presence: true
   validates :email, uniqueness: true
 
--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,6 @@ Rails.application.routes.draw do
 
   resources :relationships
 
+  post "/login", to: "auth#login"
+
 end
