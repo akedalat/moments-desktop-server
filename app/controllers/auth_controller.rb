d0b091e1bf49419f031ade905b1c1f101bbb9aed
Kian Edalat <akedalat@gmail.com>
2019-05-03 11:25:35 +0100

    added posts pictures for seeding

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,14 @@
+class AuthController < ApplicationController
+
+  def login
+    @user = User.find_by(email: params[:email])
+
+    if @user && @user.authenticate(params[:password])
+      render json: @user
+
+    else
+      render json: {errors: "login failed!"}
+    end
+  end
+
+end
