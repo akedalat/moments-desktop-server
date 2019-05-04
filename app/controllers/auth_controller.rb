12c112d43c2923d5b38cec6fbc7dd84435031734
Kian Edalat <akedalat@gmail.com>
2019-05-04 17:53:19 +0100

    auth

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -5,10 +5,6 @@ class AuthController < ApplicationController
 
     if @user && @user.authenticate(params[:password])
       render json: @user
-<<<<<<< HEAD
-
-=======
->>>>>>> master
     else
       render json: {errors: "login failed!"}
     end
