71a52f5acf4f6fa30f3791139a4e0a1a45feed1d
Kian Edalat <akedalat@gmail.com>
2019-05-07 10:01:11 +0100

    added like model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   validates :image, :user_id, presence: true
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 end
