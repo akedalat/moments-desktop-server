d462f48004eebec93e790ac2cdc0ed5c26afec84
Kian Edalat <akedalat@gmail.com>
2019-05-07 18:54:18 +0100

    like serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,6 +2,7 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :username, :email, :avatar
   has_many :posts
   has_many :comments
+  has_many :likes
   has_many :followers
   has_many :following
 end
